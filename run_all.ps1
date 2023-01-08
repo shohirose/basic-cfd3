d3dd9a9a2672203ec9ebdc29eb48880fb4b714a9
Sho Hirose <sho.hirose@gmail.com>
2023-01-08 10:15:09 +0900

    Refactor run scripts

--- run_all.ps1
+++ run_all.ps1
@@ -1,4 +1,11 @@
 $simulators = "simulator_sw_1st_order", "simulator_roe_1st_order", "simulator_lax_wendroff", "simulator_exact"
-foreach ($simulator in $simulators) {
-  & .\build\$simulator
+$buildFolders = ".\build\Release", ".\build\Debug", ".\build"
+foreach ($folder in $buildFolders) {
+  if (Test-Path $folder) {
+    foreach ($simulator in $simulators) {
+      $path = Join-Path $folder $simulator
+      & $path
+    }
+    break
+  }
 }
