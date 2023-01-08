8c05810dca3b1573e714a385b2b4a7ffd0874a04
Sho Hirose <sho.hirose@gmail.com>
2023-01-08 16:00:54 +0900

    Refactor LaxWendroffSpacialReconstructor

--- include/cfd/spacial_reconstruction_schemes.hpp
+++ include/cfd/spacial_reconstruction_schemes.hpp
@@ -60,101 +60,39 @@ class LaxWendroffSpacialReconstructor {
   template <typename Derived>
   Eigen::MatrixXd calc_flux(const Eigen::MatrixBase<Derived>& U,
                             double dt) const noexcept {
-    using Eigen::VectorXd, Eigen::MatrixXd, Eigen::seqN, Eigen::all;
-    const VectorXd rho_s =
-        calc_density_at_cell_interface(U.col(0), U.col(1), dt);
-    const VectorXd p = calc_pressure(U.col(1).array(), U.col(0).array(),
-                                     U.col(2).array(), gamma_)
-                           .matrix();
-    const VectorXd u =
-        calc_velocity(U.col(1).array(), U.col(0).array()).matrix();
-    const VectorXd rhou_s =
-        calc_momentum_density_at_cell_interface(U.col(1), u, p, dt);
-    const VectorXd et_s =
-        calc_total_energy_density_at_cell_interface(u, U.col(2), p, dt);
-    const VectorXd p_s =
-        calc_pressure(rhou_s.array(), rho_s.array(), et_s.array(), gamma_)
-            .matrix();
-    const VectorXd u_s = calc_velocity(rhou_s.array(), rho_s.array()).matrix();
-    MatrixXd F(n_domain_cells_ + 1, 3);
-    F.col(0) = rhou_s;
-    F.col(1) = (rhou_s.array() * u_s.array() + p_s.array()).matrix();
-    F.col(2) = (u_s.array() * (et_s.array() + p_s.array())).matrix();
+    using Eigen::ArrayXd, Eigen::MatrixXd, Eigen::seqN, Eigen::Map;
+
+    Map<const ArrayXd> rho(&U(0, 0), U.rows());
+    Map<const ArrayXd> rhou(&U(0, 1), U.rows());
+    Map<const ArrayXd> rhoE(&U(0, 2), U.rows());
+
+    const auto i = n_boundary_cells_;
+    const auto n = n_domain_cells_ + 1;
+    const auto rng1 = seqN(i, n);
+    const auto rng2 = seqN(i - 1, n);
+
+    const ArrayXd rho_m = 0.5 * (rho(rng1) + rho(rng2)) -
+                          (0.5 * dt / dx_) * (rhou(rng1) - rhou(rng2));
+
+    const ArrayXd u = rhou / rho;
+    const ArrayXd p = (gamma_ - 1) * (rhoE - 0.5 * rho * u.square());
+    const ArrayXd f = rhou * u + p;
+    const ArrayXd rhou_m = 0.5 * (rhou(rng1) + rhou(rng2)) -
+                           (0.5 * dt / dx_) * (f(rng1) - f(rng2));
+    const ArrayXd g = u * (rhoE + p);
+    const ArrayXd rhoE_m = 0.5 * (rhoE(rng1) + rhoE(rng2)) -
+                           (0.5 * dt / dx_) * (g(rng1) - g(rng2));
+    const ArrayXd u_m = rhou_m / rho_m;
+    const ArrayXd p_m = (gamma_ - 1) * (rhoE_m - 0.5 * rho_m * u_m.square());
+
+    MatrixXd F(n, 3);
+    F.col(0) = rhou_m;
+    F.col(1) = (rhou_m * u_m + p_m).matrix();
+    F.col(2) = (u_m * (rhoE_m + p_m)).matrix();
     return F;
   }
 
  private:
-  /**
-   * @brief Compute density at cell interfaces
-   *
-   * @tparam Derived1
-   * @tparam Derived2
-   * @param rho Density
-   * @param rhou Momentum density
-   * @param dt Time step length
-   * @return Eigen::VectorXd
-   */
-  template <typename Derived1, typename Derived2>
-  Eigen::VectorXd calc_density_at_cell_interface(
-      const Eigen::MatrixBase<Derived1>& rho,
-      const Eigen::MatrixBase<Derived2>& rhou, double dt) const noexcept {
-    using Eigen::seqN;
-    const auto i = n_boundary_cells_;
-    const auto n = n_domain_cells_ + 1;
-    return 0.5 * (rho(seqN(i, n)) + rho(seqN(i - 1, n))) -
-           (0.5 * dt / dx_) * (rhou(seqN(i, n)) - rhou(seqN(i - 1, n)));
-  }
-
-  /**
-   * @brief Compute momentum density at cell interfaces
-   *
-   * @tparam Derived1
-   * @tparam Derived2
-   * @tparam Derived3
-   * @param rhou Momentum density
-   * @param u Velocity
-   * @param p Pressure
-   * @param dt Time step length
-   * @return Eigen::VectorXd
-   */
-  template <typename Derived1, typename Derived2, typename Derived3>
-  Eigen::VectorXd calc_momentum_density_at_cell_interface(
-      const Eigen::MatrixBase<Derived1>& rhou,
-      const Eigen::MatrixBase<Derived2>& u,
-      const Eigen::MatrixBase<Derived3>& p, double dt) const noexcept {
-    using Eigen::seqN, Eigen::VectorXd;
-    const auto i = n_boundary_cells_;
-    const auto n = n_domain_cells_ + 1;
-    const VectorXd f = (rhou.array() * u.array() + p.array()).matrix();
-    return 0.5 * (rhou(seqN(i, n)) + rhou(seqN(i - 1, n))) -
-           (0.5 * dt / dx_) * (f(seqN(i, n)) - f(seqN(i - 1, n)));
-  }
-
-  /**
-   * @brief Compute total energy density at cell interfaces
-   *
-   * @tparam Derived1
-   * @tparam Derived2
-   * @tparam Derived3
-   * @param u Velocity
-   * @param et Total energy density
-   * @param p Pressure
-   * @param dt Time step length
-   * @return Eigen::VectorXd
-   */
-  template <typename Derived1, typename Derived2, typename Derived3>
-  Eigen::VectorXd calc_total_energy_density_at_cell_interface(
-      const Eigen::MatrixBase<Derived1>& u,
-      const Eigen::MatrixBase<Derived2>& et,
-      const Eigen::MatrixBase<Derived3>& p, double dt) const noexcept {
-    using Eigen::seqN, Eigen::VectorXd;
-    const auto i = n_boundary_cells_;
-    const auto n = n_domain_cells_ + 1;
-    const VectorXd f = (u.array() * (et.array() + p.array())).matrix();
-    return 0.5 * (et(seqN(i, n)) + et(seqN(i - 1, n))) -
-           (0.5 * dt / dx_) * (f(seqN(i, n)) - f(seqN(i - 1, n)));
-  }
-
   double dx_;
   double gamma_;
   int n_boundary_cells_;
