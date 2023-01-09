52c60c5c2df81c2730005f56655ae3918453a2f7
Sho Hirose <sho.hirose@gmail.com>
2023-01-09 11:51:17 +0900

    Create simulators using TVD method

---
+++ simulators/simulator_sw_tvd_minmod.cpp
@@ -0,0 +1,30 @@
+#include "cfd/cfd.hpp"
+#include "problem_settings.hpp"
+
+namespace fs = std::filesystem;
+
+using Eigen::VectorXd, Eigen::seqN;
+
+using FluxSolver = cfd::StegerWarmingRiemannSolver<
+    cfd::TvdSpacialReconstructor<cfd::MinmodLimiter>>;
+using Simulator = cfd::EulerEquationSimulator1d<FluxSolver>;
+
+Simulator make_simulator(const cfd::ProblemParameters& params) {
+  return {params, FluxSolver{params}};
+}
+
+int main(int argc, char** argv) {
+  const auto params = make_parameters();
+  const auto simulator = make_simulator(params);
+  const auto V0 = make_initial_condition(params);
+  const auto Vn = simulator.run(V0);
+
+  const auto domain = seqN(params.n_bounary_cells, params.n_domain_cells);
+  cfd::TextFileWriter writer(fs::path("result/sw_tvd_minmod"));
+  writer.write(Vn(domain, 0), "rho.txt");
+  writer.write(Vn(domain, 1), "u.txt");
+  writer.write(Vn(domain, 2), "p.txt");
+
+  const VectorXd x = make_x(params);
+  writer.write(x, "x.txt");
+}
